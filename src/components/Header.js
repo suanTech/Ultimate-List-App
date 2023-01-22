d216fdaa6f69dfa913e3ba91ae162c519b324a16
Suan Choi <siasiarhdwn@gmail.com>
2023-01-22 16:01:23 +0800

    used useEffect to get listName from localStorage

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,43 +1,42 @@
-import { useState, useEffect } from 'react'
-import { ListNameContext } from './Context';
-import { PrimaryButton } from './styles/Buttons.styled';
-import { EditInput } from './styles/Header.styled';
-import { ListName } from './styles/Text.styled';
+import { useState, useEffect } from "react";
+import { PrimaryButton } from "./styles/Buttons.styled";
+import { EditInput } from "./styles/Header.styled";
+import { ListName } from "./styles/Text.styled";
 
-export default function Header({initialName}) {
-  const [listName, setListName] = useState(() => {
-    const savedName = localStorage.getItem("list-name");
-    const newName = JSON.parse(savedName);
-    return newName ? newName : initialName;
-  });
+export default function Header({ initialName }) {
+  const [listName, setListName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
   useEffect(() => {
     localStorage.setItem("list-name", JSON.stringify(listName));
   }, [listName]);
+  useEffect(() => {
+    const listName = JSON.parse(localStorage.getItem('list-name'))
+    if(listName) {
+      setListName(listName)
+    }
+  },[])
   return (
-    <ListNameContext.Provider value={listName}>
-    {isEditing ? 
-      <>
-        <EditInput 
-          type='text' 
-          value={listName}
-          onChange={(e)=> setListName(e.target.value)}
-          style={{width: `${listName.length}ch`}}
-        />
-        <PrimaryButton
-        onClick={() => setIsEditing(!isEditing)}>
-        Save</PrimaryButton> 
-      </>
-      : 
-      <>
-        <ListName> {listName} </ListName>
-        <PrimaryButton
-      onClick={() => setIsEditing(!isEditing)}>
-      Edit Name {'\u270D'}</PrimaryButton>
-      </>
-      }
-      {/* when button clicks(edit mode) => */}
-      {/* <input type='text' value="Todo List"/> */}
-    </ListNameContext.Provider>
-  )
+    <>
+      {isEditing ? (
+        <>
+          <EditInput
+            type="text"
+            value={listName}
+            onChange={(e) => setListName(e.target.value)}
+            style={{ width: `${listName.length}ch` }}
+          />
+          <PrimaryButton onClick={() => setIsEditing(!isEditing)}>
+            Save
+          </PrimaryButton>
+        </>
+      ) : (
+        <>
+          <ListName> {listName} </ListName>
+          <PrimaryButton onClick={() => setIsEditing(!isEditing)}>
+            Edit Name {"\u270D"}
+          </PrimaryButton>
+        </>
+      )}
+    </>
+  );
 }
